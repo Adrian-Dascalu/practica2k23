abe0d01137c290062e1b089a5258bf763fff7337
Adrian <adiadrian9798@gmail.com>
2023-06-25 18:25:56 +0300

    hello world mp3

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("goodbyw world")
+import pyttsx3
+engine = pyttsx3.init()
+hello = "hello world"
+engine.say(hello)
+engine.save_to_file(hello, 'hello.mp3')
+engine.runAndWait()
+
+from sympy.solvers import solve
+from sympy import Symbol
+x = Symbol('x')
+print(solve(x**2 - 1, x))
